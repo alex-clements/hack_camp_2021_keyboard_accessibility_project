2128d19713e54fadcf431f74316154af5db36994
alex-clements <ahclemen@ualberta.ca>
2021-11-14 17:00:06 -0800

    debounced mutation fucntion attempt #1

---
+++ test.js
@@ -0,0 +1,11 @@
+// import './mapLayout.js';
+
+// window.onload = () => {
+//     var mutationObserver = new MutationObserver(function(mutations) {
+//         mutations.forEach(function(mutation) {
+//             console.log(mutation);
+//         })
+//     });
+
+//     mutationObserver.observe(document.body, {attributes: false, subtree: true, childList: true, characterData: true});
+// }
