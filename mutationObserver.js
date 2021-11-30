a11eba6d21f29abe5614de328acccabeb1cb5fc9
alex-clements <ahclemen@ualberta.ca>
2021-11-29 17:10:32 -0800

    added debounced graphLayout function

--- mutationObserver.js
+++ mutationObserver.js
@@ -1,7 +1,28 @@
 function setupMutationObserver() {
     var mutationObserver = new MutationObserver(function(mutations) {
-        mutations.forEach(graphLayout)
+        mutations.forEach(debounce(graphLayout,300))
     });
     mutationObserver.observe(document.body, {attributes: false, subtree: true, childList: true, characterData: true});
 
+}
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    
+    return function executedFunction() {
+        var context = this;
+        var args = arguments;
+
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(later, wait)
+
+        if(callNow) func.apply(context, args);
+    }
 }
